a4e9565105b71d12b5039f6571755a5f17f0714a
Amin <ameri.env@gmail.com>
2017-08-16 21:55:47 -0400

    apidoc for /signup was added

--- server/server.js
+++ server/server.js
@@ -96,6 +96,60 @@ app.post('/api/v2/forgot-password', (req, res) => {
     let {username} = _.pick(req.body, ["username"]);
     res.send('Received');
 });
+
+
+/**
+ * @api {POST} /api/v2/signup Sign Up
+ * @apiGroup Authentication
+ * @apiVersion 2.0.0
+ * @apiName SignUp
+ * @apiDescription This API is used for Simple Sign Up
+ * @apiParam {string} name name of the user
+ * @apiParam {string} username username of the user
+ * @apiParam {string} password password of the user
+ * @apiHeader {string} Content-Type value should be <code>application/json</code>
+ * @apiHeaderExample {string} Header-Example:
+ * "Content-Type":"application/json"
+ * @apiParamExample {json} Request-Sample:
+ * {
+ *  "name": "user",
+ *  "username": "user@test.com",
+ *  "password": "123456"
+ * }
+ * @apiSuccess (Success-Response-body) {json} user User Object is return in the body
+ * @apiSuccess (Success-Response-body) {json} communities Communities Object is return in the body
+ * @apiSuccess (Success-Response-header) {string} token is returned in hte respons header
+ * @apiSuccessExample {json} Success-Response-Body:
+ * {
+ *  "user": {
+ *      //LOGGED IN USER DETAILS
+ *      },
+ *  "communities": [{
+ *      //USER'S FIRTS COMMUNITY
+ *  }, {
+ *      //USER'S SECOND COMMUNITY
+ *  }]
+ * }
+ * @apiSuccessExample {string} Success-Response-Header:
+ * token: STRING_OF_TOKEN 
+ * @apiError (401) {json} WRONG_EMAIL username was wrong
+ * @apiError (401) {json} EMPTY_NAME name was empty
+ * @apiError (401) {json} BAD_PASSWORD password did not follow the policy
+ * @apiError (404) {json} PAGE_NOT_FOUND check the URL
+ * @apiError (500) {json} NETWROK_ISSUE check the network
+ * 
+ * @apiErrorExample {json} Error-Response-Body:
+ * {
+ *  "error": {
+ *      "code": "SOME_CODE",
+ *      "message": "SOME_MESSAGE"
+ *  }
+ * }
+ */
+app.post('/api/v2/signup', (req, res) => {
+    let {name, username, password} = _.pick(req.body, ["name", "username", "password"]);
+    res.send('Received');
+});
  
 app.listen(port, () => console.log(`Server started and listening to Port ${port}`));
  
