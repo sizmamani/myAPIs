6fb1fc1924e4c88d2d910a064d034643e20e71ae
Amin <ameri.env@gmail.com>
2017-08-29 18:41:08 -0400

    GET /users/:id and its test cases done

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 const { mongoose } = require('../db/mongoose.js');
 const { User } = require('../models/user');
-//const { generateError, RESPONSE_CODES, ERRORS, MESSAGES } = require('../utils/message.util');
+const { RESPONSE_CODES, ERRORS, MESSAGES } = require('../utils/message.util');
 const { authenticate } = require('../middlewares/authenticate');
 const router = express.Router();
 
@@ -56,28 +56,25 @@ router.get('/me', authenticate, (req, res) => {
  * @apiGroup Users
  * @apiVersion 2.0.0
  * @apiName GetUserInfoById
- * @apiDescription This API is used to get sdasdsd
+ * @apiDescription This API is used to get User object by their id
  * @apiHeaderExample {string} Header-Example:
  * "Content-Type":"application/json"
  * "token":"TOKEN_GIVEN_WHEN_LOGIN"
  * @apiSuccess (Success-Response-body) {json} user User Object is return in the body
- * @apiSuccess (Success-Response-body) {json} communities Communities Object is return in the body
  * @apiSuccessExample {json} Success-Response-Body:
  * {
  *  "user": {
  *      "_id": "59a3699ff3517345c03faddf",
  *      "firstName": "John",
  *      "lastName": "Smith",
- *      "loginId": "john-smith@test.com"
- *  },
- *  "communities": [{
- *      //USER'S FIRTS COMMUNITY
- *  }, {
- *      //USER'S SECOND COMMUNITY
- *  }]
+ *      "loginId": "john-smith@test.com",
+ *      "email": "john-smith@test.com",
+ *  }
  * }
  * @apiError (401) {json} WRONG_TOKEN TOKEN WAS WRONG
  * @apiError (401) {json} EXPIRED_TOKEN TOKEN WAS EXPIRED
+ * @apiError (401) {json} WRONG_TOKEN INVALID USER ID
+ * @apiError (401) {json} WRONG_TOKEN USER DOES NOT EXIST
  * @apiError (404) {json} PAGE_NOT_FOUND check the URL
  * @apiError (500) {json} NETWROK_ISSUE check the network
  * 
@@ -89,8 +86,19 @@ router.get('/me', authenticate, (req, res) => {
  *  }
  * }
  */
-router.get('/me', authenticate, (req, res) => {
-
+router.get('/:id', authenticate, async (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(RESPONSE_CODES.UNAUTHORIZED).send(ERRORS.INVALID_USER_ID);
+    }
+    let user = await User.findById({
+        _id: id
+    });
+    if(user){
+        return res.send({user});
+    }else{
+        return res.status(RESPONSE_CODES.UNAUTHORIZED).send(ERRORS.USER_DOES_NOT_EXIST);
+    }
 });
 
 module.exports = router;
--- server/tests/users.test.js
+++ server/tests/users.test.js
@@ -7,42 +7,37 @@ const { users, populateUsers } = require('./seed/user_seed');
 const { generateError, RESPONSE_CODES, ERRORS, MESSAGES } = require('../utils/message.util');
 const { User } = require('../models/user');
 const tokenUtil = require('../utils/token.util');
+const _ = require('lodash');
 
 const URL = '/api/v2/users';
 
 beforeEach(populateUsers);
 
 describe('Users Tests', () => {
+    user = _.omit(users[0], ['password']);
+    let data = {
+        _id: users[0]._id,
+        user
+    };
+    let token = tokenUtil.generateToken(data);
     describe('GET /users/me', () => {
         it('should return the user info which is logged in with its token', (done) => {
-            let data = {
-                _id: '59a5c3874fb26705969c6f47',
-                user:
-                {
-                    _id: '59a5c3874fb26705969c6f47',
-                    firstName: 'Sherlock',
-                    lastName: 'Holmes',
-                    loginId: 'sherlock@221B.baker.str',
-                    email: 'sherlock@221B.baker.str',
-                    status: 1,
-                    myInterests: [],
-                    myExpertise: []
-                }
-            };
-            let token = tokenUtil.generateToken(data);
             request(app)
                 .get(`${URL}/me`)
                 .set('token', token)
                 .expect(200)
                 .expect((res) => {
                     expect(res.body.user).toExist();
-                    expect(res.body.user).toInclude(data.user);
+                    expect(res.body.user._id).toEqual(user._id);
+                    expect(res.body.user.firstName).toBe(user.firstName);
+                    expect(res.body.user.loginId).toBe(user.loginId);
                 })
                 .end(done);
         });
         it('should reject user with wrong token', (done) => {
             request(app)
                 .get(`${URL}/me`)
+                .set('token', token + 'A')
                 .expect(RESPONSE_CODES.UNAUTHORIZED)
                 .expect((res) => {
                     expect(res.body.error).toExist();
@@ -53,4 +48,40 @@ describe('Users Tests', () => {
                 .end(done);
         });
     });
+    describe('GET /users/:id', () => {
+        it('should return user with a given id', (done) => {
+            request(app)
+                .get(`${URL}/${users[0]._id.toHexString()}`)
+                .set('token', token)
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.user).toExist();
+                    expect(res.body.user._id).toEqual(users[0]._id);
+                    expect(res.body.user.firstName).toBe(user.firstName);
+                    expect(res.body.user.loginId).toBe(user.loginId);
+                })
+                .end(done);
+        });
+        it('should return error with a valid id but not record', (done) => {
+            request(app)
+            .get(`${URL}/${new ObjectID()}`)
+            .set('token', token)
+            .expect(RESPONSE_CODES.UNAUTHORIZED)
+            .expect((res) => {
+                expect(res.body.error).toExist();
+                expect(res.body.error).toInclude(ERRORS.USER_DOES_NOT_EXIST.error);
+                expect(res.body.error.code).toBe(ERRORS.USER_DOES_NOT_EXIST.error.code);
+                expect(res.body.error.message).toBe(ERRORS.USER_DOES_NOT_EXIST.error.message);
+            })
+            .end(done);
+        });
+        it('should reject wrong id', (done) => {
+            request(app)
+            .get(`${URL}/1234`)
+            .set('token', token)
+            .expect(RESPONSE_CODES.UNAUTHORIZED)
+            .end(done);
+        });
+
+    })
 });
